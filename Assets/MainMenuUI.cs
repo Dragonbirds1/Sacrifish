b53d79f643b4da9319e03581264b6558efb7c6b3
MUKILTEO\1549673 <1549673>
2026-03-16 13:14:47 -0700

    Made Main Menu almost completely work

---
+++ Assets/MainMenuUI.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuUI : MonoBehaviour
+{
+    public void Start()
+    {
+        //Starts Game
+        SceneManager.LoadScene("Game");
+    }
+    public void Quit()
+    {
+        //Closes The Game
+    }
+}
