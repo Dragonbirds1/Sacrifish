b141bfdea8b07c0ceb896c90e44a82cccbba80dd
MUKILTEO\1549673 <1549673>
2026-03-19 13:19:21 -0700

    progress on item display

--- Assets/InventoryUI.cs
+++ Assets/InventoryUI.cs
@@ -5,10 +5,13 @@ public class InventoryUI : MonoBehaviour
 {
     public float Rows;
     public float Collumns;
-    public float CellSize;
+    public float CellSize = 150;
+    public float CellSpacing = 25;
+    public float CanvasSize = 1920;
     // Update is called once per frame
     void Update()
     {
-        
+        RectTransform rectTransform = GetComponent<RectTransform>();
+        //rectTransform.rect.left = (CanvasSize - (CellSize * Collumns + CellSpacing * (Collumns - 1))) / 2;
     }
 }
