4f4138b0038fe72a30c91d8c0fb4d5e19bea7148
MUKILTEO\1549673 <1549673>
2026-03-24 13:18:59 -0700

    hotbar work in progress

--- Assets/InventoryUI.cs
+++ Assets/InventoryUI.cs
@@ -5,18 +5,26 @@ using UnityEngine.UI;
 public class InventoryUI : MonoBehaviour
 {
     public uint Rows;
-    public uint Collumns;
+    public float Collumns;
     private void Start()
     {
         GridLayoutGroup gridLayoutGroup = GetComponent<GridLayoutGroup>();
 
         RectTransform rectTransform = GetComponent<RectTransform>();
-        rectTransform.sizeDelta = new Vector2(Collumns * gridLayoutGroup.cellSize.x + gridLayoutGroup.spacing.x * (Collumns + 1), Collumns * gridLayoutGroup.cellSize.x + gridLayoutGroup.spacing.x * (Collumns + 1));
+        rectTransform.sizeDelta = new Vector2(Rows * gridLayoutGroup.cellSize.x + gridLayoutGroup.spacing.x * (Rows + 1), Rows * gridLayoutGroup.cellSize.x + gridLayoutGroup.spacing.x * (Rows + 1));
     }
     // Update is called once per frame
     public void Update()
     {
-        
+        if (Mathf.Round(transform.childCount / Rows) != (transform.childCount / Rows))
+        {
+            //Collumns = Mathf.Round(transform.childCount / Rows) + 1;
+            Collumns = (transform.childCount / (float)Rows) + 1;
+        }
+        else
+        {
+            Collumns = (transform.childCount / (float)Rows);
+        }
     }
     /*public void EXIT()
     {
