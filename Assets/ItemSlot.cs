3826196a812f352ea05d429b4c83734956c8c539
MUKILTEO\1549673 <1549673>
2026-03-25 13:20:14 -0700

    item display finished, hotbar finished, buttons in progress, & item stacking finished

--- Assets/ItemSlot.cs
+++ Assets/ItemSlot.cs
@@ -1,14 +1,18 @@
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class ItemSlot : MonoBehaviour
+public class ItemSlot : MonoBehaviour, IPointerClickHandler
 {
     //======ITEM DATA======//
     public string itemName;
     public int quantity;
     public Sprite itemSprite;
     public bool isFull;
+
+    [SerializeField]
+    private int maxNumberOfItems = 99;
 
     //====ITEM SLOT====//
     [SerializeField]
@@ -18,15 +22,73 @@ public class ItemSlot : MonoBehaviour
     private Image itemImage;
 
 
-    public void AddItem(string itemName, int quantity, Sprite itemSprite)
-    {
-        this.itemName = itemName;
-        this.quantity = quantity;
-        this.itemSprite = itemSprite;
-        isFull = true;
+    public GameObject selectedShader;
+    public bool thisItemSelected;
 
-        quantityText.text = quantity.ToString();
-        quantityText.enabled = true;
+    private InventoryManager inventoryManager;
+
+    private void Start()
+    {
+        inventoryManager = GameObject.Find("Inventory Canvas").GetComponent<InventoryManager>();
+    }
+
+
+
+    public int AddItem(string itemName, int quantity, Sprite itemSprite)
+    {
+        //Check to see if the slot is already full
+        if ( isFull)
+            return quantity;
+
+        //Update NAME
+        this.itemName = itemName;
+
+        //Update image
+        this.itemSprite = itemSprite;
         itemImage.sprite = itemSprite;
+
+        //Update QUANTITY
+        this.quantity += quantity;
+        if (this.quantity >= maxNumberOfItems)
+        {
+            quantityText.text = maxNumberOfItems.ToString();
+            quantityText.enabled = true;
+            isFull = true;
+
+            //Return the LEFTOVERS
+            int extraItems = this.quantity - maxNumberOfItems;
+            this.quantity = maxNumberOfItems;
+            return extraItems;
+        }
+
+        //Update QUANTITY TEXT
+        quantityText.text = this.quantity.ToString();
+        quantityText.enabled = true;
+
+        return 0;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (eventData.button == PointerEventData.InputButton.Left)
+        {
+            OnLeftClick();
+        }
+        if (eventData.button == PointerEventData.InputButton.Right)
+        {
+            OnRightClick();
+        }
+    }
+    public void OnLeftClick()
+    {
+        inventoryManager.DeselectAllSlots();
+        selectedShader.SetActive(true);
+        thisItemSelected = true;
+    }
+
+
+    public void OnRightClick()
+    {
+
     }
 }
