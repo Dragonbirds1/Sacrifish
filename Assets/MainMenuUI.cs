4171d2b8100273df50c92623b1dac7a8d934480c
MUKILTEO\1725593 <1725593@Mukilteo.wednet.edu>
2026-05-28 13:24:10 -0700

    Loading Screen

--- Assets/MainMenuUI.cs
+++ Assets/MainMenuUI.cs
@@ -1,12 +1,60 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class MainMenuUI : MonoBehaviour
 {
+    public Animator loadingAnim;
+    public float waitTime, updateTime;
+    public GameObject loadingScreen;
+    public TextMeshProUGUI loadingText;
+    public string loadingTextUpdater;
+    public bool startLoading, startCountdown;
+
+    private void Update()
+    {
+        if (startLoading)
+        {
+            loadingAnim.SetBool("StartSpin", true);
+            loadingText.text = loadingTextUpdater;
+            updateTime += Time.deltaTime;
+            if (updateTime >= 0f)
+            {
+                loadingTextUpdater = "Loading";
+            }
+            if (updateTime >= 0.5f)
+            {
+                loadingTextUpdater = "Loading.";
+            }
+            if (updateTime >= 1f)
+            {
+                loadingTextUpdater = "Loading..";
+            }
+            if (updateTime >= 1.5f)
+            {
+                loadingTextUpdater = "Loading...";
+            }
+            if (updateTime >= 2f)
+            {
+                updateTime = 0f;
+            }
+        }
+        if (startCountdown)
+        {
+            waitTime += Time.deltaTime;
+            if (waitTime >= 5f)
+            {
+                SceneManager.LoadScene("Map");
+                waitTime = 0f;
+            }
+        }
+    }
     public void Play()
     {
         //Starts Game
-        SceneManager.LoadScene("Map");
+        loadingScreen.SetActive(true);
+        startLoading = true;
+        startCountdown = true;
     }
     public void Quit()
     {
