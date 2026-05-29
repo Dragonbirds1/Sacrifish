93475aa1e6a41ce170cd3312104a322e7ff06b47
MUKILTEO\1725593 <1725593@Mukilteo.wednet.edu>
2026-05-29 12:55:13 -0700

    Bobber and Boat fixed

--- Assets/Scripts/BoatScripts/BoatManager.cs
+++ Assets/Scripts/BoatScripts/BoatManager.cs
@@ -12,7 +12,10 @@ public class BoatManager : MonoBehaviour
 
     public GameObject boat;
 
-    public CharacterController boatController;
+    //public CharacterController boatController;
+
+    // Stop using CharacterController.Move so the boat can float on water and not be affected by gravity when in the boat.
+    public Rigidbody boatRigidbody;
 
     public Transform boatSeat;
 
@@ -21,6 +24,8 @@ public class BoatManager : MonoBehaviour
     public float speed = 5f;
 
     public float currentSpeed;
+
+    public float acceleration; // This is for the boat to gradually speed up and slow down instead of instantly changing speed.
 
     public float gravity = -9.8f;
 
@@ -31,6 +36,7 @@ public class BoatManager : MonoBehaviour
     public Vector3 moveDirection = Vector3.zero;
 
     private Vector3 playerVelocity;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -70,13 +76,19 @@ public class BoatManager : MonoBehaviour
         {
             moveDirection.x = -input.y;
             moveDirection.z = input.x;
-            boatController.Move(transform.TransformDirection(moveDirection) * speed * Time.deltaTime);
+            //boatController.Move(transform.TransformDirection(moveDirection) * speed * Time.deltaTime);
+            // Stop using CharacterController.Move so the boat can float on water and not be affected by gravity when in the boat.
+            // Make it so force mode acceleration is the acceleration we set in the inspector so the boat can gradually speed up and slow down instead of instantly changing speed.
+            // make it speed up and slow down based on the acceleration variable we set in the inspector instead of instantly changing speed.
+            boatRigidbody.AddForce(transform.TransformDirection(moveDirection) * speed * Time.deltaTime, ForceMode.Acceleration);
             // Update current Speed based on the player's movement input
             currentSpeed = moveDirection.magnitude * speed;
             playerVelocity.y += gravity * Time.deltaTime;
             //if (isGrounded && playerVelocity.y < 0)
                 //playerVelocity.y = -2f;
-            boatController.Move(playerVelocity * Time.deltaTime);
+            //boatController.Move(playerVelocity * Time.deltaTime);
+            // Do the same here
+            //boatRigidbody.AddForce(Vector3.up * playerVelocity.y, ForceMode.Acceleration);
             //Debug.Log(playerVelocity.y);
         }
     }
