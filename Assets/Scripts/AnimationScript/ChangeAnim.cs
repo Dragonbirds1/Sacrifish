e8e39d41964adbcb8d4f2cb10e14c390f4806f25
DRAGONBIRDS\drago <dragonbirds735@gmail.com>
2026-06-16 11:57:41 -0700

    Fixes and Fixed the Randomize Animation script

--- Assets/Scripts/AnimationScript/ChangeAnim.cs
+++ Assets/Scripts/AnimationScript/ChangeAnim.cs
@@ -2,112 +2,61 @@ using UnityEngine;
 
 public class ChangeAnim : MonoBehaviour
 {
-    public Animator playerAnim, otherAnim;
+    public Animator playerAnim;
+    public Animator otherAnim;
 
-    public bool isPlayerAnim, isOtherAnim, timerStart;
+    private float timer;
+    private bool playing;
 
-    public bool hatToggle, sharkToggle, scareToggle;
-
-    public float timer;
-
-    public float HatTimer, SharkTimer, ScareTimer;
-
-    public int animNumber;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        isPlayerAnim = true;
-        timerStart = true;
+        // Force both to idle at start
+        playerAnim.Play("Idle", 0, 0f);
+        otherAnim.Play("Idle", 0, 0f);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (isPlayerAnim)
-        {
-            playerAnim.enabled = true;
-            otherAnim.enabled = false;
-        }
-        else
-        {
-            otherAnim.enabled = true;
-            playerAnim.enabled = false;
-        }
+        if (playing) return;
 
-        if (timerStart)
-        {
-            isPlayerAnim = true; // Make the player idle animation play
-            playerAnim.SetBool("PlayerReturn", false); // Reset the player return bool to false so the idle animation can play
-            otherAnim.SetBool("Return", false); // Reset the other return bool to false so the idle animation can play
-            timer += Time.deltaTime;
-            if (timer >= 10f)
-            {
-                animNumber = Random.Range(0, 2); // Randomly select ethier 0, 1, or 2 for three different animations
-                if (animNumber == 0)
-                {
-                    isPlayerAnim = true;
-                    hatToggle = true;
-                    sharkToggle = false;
-                    scareToggle = false;
-                }
-                else if (animNumber == 1)
-                {
-                    isPlayerAnim = false;
-                    sharkToggle = true;
-                    scareToggle = false;
-                    hatToggle = false;
-                }
-                else if (animNumber == 2)
-                {
-                    isPlayerAnim = false;
-                    scareToggle = true;
-                    hatToggle = false;
-                    sharkToggle = false;
-                }
-                timer = 0f;
-                timerStart = false;
-            }
-        }
+        timer += Time.deltaTime;
 
-        if (hatToggle)
+        if (timer >= 10f)
         {
-            playerAnim.SetBool("HatAnim", true);
-            HatTimer += Time.deltaTime;
-            if (HatTimer >= 11f)
+            timer = 0f;
+            playing = true;
+
+            int anim = Random.Range(0, 3);
+
+            if (anim == 0)
             {
-                hatToggle = false;
-                playerAnim.SetBool("HatAnim", false);
-                playerAnim.SetBool("PlayerReturn", true);
-                HatTimer = 0f;
-                timerStart = true;
+                playerAnim.SetBool("HatAnim", true);
+                Invoke(nameof(EndAnim), 11f);
+            }
+            else if (anim == 1)
+            {
+                otherAnim.SetBool("SharkAnim", true);
+                Invoke(nameof(EndAnim), 23f);
+            }
+            else
+            {
+                otherAnim.SetBool("ScareAnim", true);
+                Invoke(nameof(EndAnim), 6f);
             }
         }
-        else if (sharkToggle)
-        {
-            otherAnim.SetBool("SharkAnim", true);
-            SharkTimer += Time.deltaTime;
-            if (SharkTimer >= 23f)
-            {
-                sharkToggle = false;
-                otherAnim.SetBool("SharkAnim", false);
-                otherAnim.SetBool("Return", true);
-                SharkTimer = 0f;
-                timerStart = true;
-            }
-        }
-        else if (scareToggle)
-        {
-            otherAnim.SetBool("ScareAnim", true);
-            ScareTimer += Time.deltaTime;
-            if (ScareTimer >= 6f)
-            {
-                scareToggle = false;
-                otherAnim.SetBool("ScareAnim", false);
-                otherAnim.SetBool("Return", true);
-                ScareTimer = 0f;
-                timerStart = true;
-            }
-        }
+    }
+
+    void EndAnim()
+    {
+        // TURN OFF ALL ANIMATIONS (critical fix)
+        playerAnim.SetBool("HatAnim", false);
+        otherAnim.SetBool("SharkAnim", false);
+        otherAnim.SetBool("ScareAnim", false);
+
+        // FORCE BOTH BACK TO IDLE (this is what you were missing)
+        playerAnim.Play("Idle", 0, 0f);
+        otherAnim.Play("Idle", 0, 0f);
+
+        playing = false;
     }
 }
